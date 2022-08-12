44b42dbf7b280fa228182c02290b5b2cabba591f
Olaotan Abarowei <olaotanabarowei@gmail.com>
2022-08-12 20:04:51 +0100

    fixed bug

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -33,7 +33,7 @@ const Home = () => {
 
   const genres = ["Crime", "Thriller", "Neo-Western", "Tragedy"];
   const description =
-    "Breaking Bad follows protagonist Walter White, a chemistry teacher who lives in New Mexico with his wife and teenage son who has cerebral palsy. White is diagnosed with Stage III cancer and given a prognosis of two years left to live. With a new sense of fearlessness based on his medical prognosis, and a desire to secure his family's financial security, White chooses to enter a dangerous world of drugs and crime and ascends to power in this world. The series explores how a fatal diagnosis such as White's releases a typical man from the daily concerns and constraints of normal society and follows his transformation from mild family man to a kingpin of the drug trade.";
+    "Breaking Bad follows protagonist Walter White, a chemistry teacher who lives in New Mexico with his wife and teenage son who has cerebral palsy. White is diagnosed with Stage III cancer and given a prognosis of two years left to live. With a new sense of fearlessness based on his medical prognosis, and a desire to secure his family's financial security, White chooses to enter a dangerous world of drugs and crime and ascends to power in this world.";
   const rating = 9.2;
 
   return (
