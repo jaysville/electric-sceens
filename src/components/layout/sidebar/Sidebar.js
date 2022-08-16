2bd2492f9f82fdf898599bacdc09ad5cf72854df
Olaotan Abarowei <olaotanabarowei@gmail.com>
2022-08-16 19:09:50 +0100

    fixed error

--- src/components/layout/sidebar/Sidebar.js
+++ src/components/layout/sidebar/Sidebar.js
@@ -1,8 +1,11 @@
 import classes from "./Sidebar.module.css";
 import sidebarData from "./SidebarData";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Sidebar = (props) => {
+  const [isActive, setIsActive] = useState(window.location.pathname);
+
   return (
     <nav
       className={`${classes.sidebar} ${
@@ -15,16 +18,20 @@ const Sidebar = (props) => {
       <ul className={classes["sidebar-list"]}>
         {sidebarData.map(({ title, icon, link }, i) => {
           return (
-            <NavLink
-              activeClassName={classes.active}
+            <Link
               style={{ textDecoration: "none" }}
               to={link}
               key={i}
               onClick={() => {
+                setIsActive(link);
                 props.onHideNav();
               }}
             >
-              <li className={classes.row}>
+              <li
+                className={`${classes.row} ${
+                  isActive === link && classes.active
+                }`}
+              >
                 <div className={classes["sidebar-icon"]}>{icon}</div>
                 <div
                   className={` animate__animated animate__fadeInUp ${classes["sidebar-title"]} `}
@@ -32,7 +39,7 @@ const Sidebar = (props) => {
                   {title}
                 </div>
               </li>
-            </NavLink>
+            </Link>
           );
         })}
       </ul>
